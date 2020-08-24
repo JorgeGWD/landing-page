117bca3beae7e59b5ab816b22f2fe259ba3fde7a
jorgegwd <jorge.rodriguez.r@gmail.com>
2020-08-24 14:32:43 -0500

    Welcome text style

--- components/welcome/welcome.js
+++ components/welcome/welcome.js
@@ -12,8 +12,9 @@ const Welcome = () => {
             <div className="welcome-content">
                 <div className="welcome-text">
                     <h1>¿Qué parte de tu cuerpo deseas mejorar hoy?</h1>
-                    <p>Atendemos más de 47 tratamientos para 58 soluciones corporales y faciales en 15 años de experiencia como IPS en estética.</p>
-                    <p>Inicia hoy tu proyecto de mejoramiento facial y corporal financiando tu tratamiento en cuotas sin intereses.</p>
+                    <hr/>
+                    <p>Atendemos más de <span>47 tratamientos para 58 soluciones</span> corporales y faciales en 15 años de experiencia como IPS en estética.</p>
+                    <p>Inicia hoy tu proyecto de mejoramiento facial y corporal financiando <span>tu tratamiento en cuotas sin intereses</span>.</p>
                 </div>
                 <div className="form">
 
