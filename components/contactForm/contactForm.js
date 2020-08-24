4a6db8e1374f2119d137a14a996d0f91c75e8a2e
jorgegwd <jorge.rodriguez.r@gmail.com>
2020-08-24 15:30:32 -0500

    Welcome text align

--- components/contactForm/contactForm.js
+++ components/contactForm/contactForm.js
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './contactForm.css'
 
 const ContactForm = () => {
+
+    const [ data, setData ] = useState({
+        name: '',
+        phone: '',
+        email: '',
+        selector: '',
+        checkbox: false
+    })
+
+    const handleChange = (e) => {
+        console.log(e.target.value)
+
+        setData({
+            ...data,
+            [e.target.value] : e.target.name
+        })
+    }
     return (
         <div className="contact-form">
             <h2>Escríbenos y obtén asesoría inicial personalizada y sin costo</h2>
             <form action="">
-                <input type="text" name="name" placeholder="Nombre completo"/>
-                <input type="text" name="phone" placeholder="Teléfono"/>
-                <input type="email" name="email" placeholder="E-mail"/>
+                <input type="text" name="name" placeholder="Nombre completo" onChange={handleChange} />
+                <input type="text" name="phone" placeholder="Teléfono" onChange={handleChange} />
+                <input type="email" name="email" placeholder="E-mail" onChange={handleChange} />
                 <select name="selector">
                     <option value="">Elija área a tratar</option>
                     <option value="">Estética facial</option>
--- components/welcome/welcome.js
+++ components/welcome/welcome.js
@@ -11,11 +11,13 @@ const Welcome = () => {
     return (
         <div className="welcome">
             <div className="welcome-content">
-                <div className="welcome-text">
-                    <h1>¿Qué parte de tu cuerpo deseas mejorar hoy?</h1>
-                    <hr/>
-                    <p>Atendemos más de <span>47 tratamientos para 58 soluciones</span> corporales y faciales en 15 años de experiencia como IPS en estética.</p>
-                    <p>Inicia hoy tu proyecto de mejoramiento facial y corporal financiando <span>tu tratamiento en cuotas sin intereses</span>.</p>
+                <div className="text-panel">
+                    <div className="welcome-text">
+                        <h1>¿Qué parte de tu cuerpo deseas mejorar hoy?</h1>
+                        <hr/>
+                        <p>Atendemos más de <span>47 tratamientos para 58 soluciones</span> corporales y faciales en 15 años de experiencia como IPS en estética.</p>
+                        <p>Inicia hoy tu proyecto de mejoramiento facial y corporal financiando <span>tu tratamiento en cuotas sin intereses</span>.</p>
+                    </div>
                 </div>
                 <div className="form">
                     <ContactForm />
