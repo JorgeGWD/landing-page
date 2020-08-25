d9fb04db85c53606e2485c4ad494c516d82f8609
jorgegwd <jorge.rodriguez.r@gmail.com>
2020-08-25 09:22:06 -0500

    Head tag

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+
+import Head from 'next/head'
 import Header from '../components/header/header'
 import './sass/styles.css'
 import Welcome from '../components/welcome/welcome'
@@ -5,6 +7,10 @@ import OurAdvantages from '../components/ourAdvantages/ourAdvantages'
 
 const IndexPage = () => (
   <>
+    <Head>
+      <title>Cifes | Soluciones estéticas</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
     <Header />
     <Welcome />
     <OurAdvantages />
