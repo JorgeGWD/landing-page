0d5bff4f2db416fde64967e664f7ca6c9b2a85b2
jorgegwd <jorge.rodriguez.r@gmail.com>
2020-08-26 16:48:29 -0500

    facialAesthetic data

--- components/facialAesthetics/facialAesthetics.js
+++ components/facialAesthetics/facialAesthetics.js
@@ -6,34 +6,59 @@ const FacialAesthetics = () => {
 
     const TABS = [
         { 
-            label: "Tab 1", value: 1
+            label: "Eliminación de arrugas",
+            value: 1,
+            title: "Líneas de expresión, arrugas profundas y finas",
+            text: "Tratamiento para surco nasogeniano, líneas marioneta, arrugas de frente, ojos, boca con láser CO2, toxina botulínica, ácido hialurónico, plasma, dermapen e hilos tensores."
         },
         {
-            label: "Tab 2", value: 2
+            label: "Perfilamiento facial y flacidez",
+            value: 2,
+            title: "Ojeras, flacidez facial moderada a severa",
+            text: "Tratamiento para ojeras, papada, perfilamiento de labios, pómulos, mentón, flacidez en cuello y manos, rinomodelación (perfilamiento de nariz) con hidroxiapatita de calcio, vital firm, radiofrecuencia 3G,  plasma e hilos tensores entre otros. "
         },
         {
-            label: "Tab 3", value: 3
+            label: "Eliminación de manchas",
+            value: 3,
+            title: "Manchas, melasmas, pecas y lunares",
+            text: "ratamiento para Manchas rojas, manchas marrones difusas y extensas pecas y lunares con peeling, laser tatoo, cosmelan  y láser de CO2."
+        },
+        {
+            label: "Eliminación de Acné",
+            value: 4,
+            title: "Acné leve, severo y cicatrices",
+            text: "Tratamiento para Acné comedogénico, conglobata y cicatrices profundas con autosanguis, biorevitalización, detoxificación y láser de CO2 fraccionado."
         }
       ];
 
-    const [activeTab, handleTab] = useState(1);
+    const [ activeTab, handleTab ] = useState(1);
 
     return (
         <div className="container">
             <div className="content">
                 <div className="content-data">
-                    <h1>Estetica Facial</h1>
+                    <h1>Estética Facial</h1>
                     <hr/>
+                    <div className="treatment-data">
+                        <p>37 Tratamientos | 21 Zonas de tu cara a tratar</p>
+                        <h3>Selecciona tu área de interés:</h3>
+                    </div>
                     <div className="tabs-container">
                         <TabManager tabs={TABS} activeTab={activeTab} handleTab={handleTab} />
                         <div className="tab-content">
-                            {" "}
-                            <div> Content of Tab {activeTab}</div>
+                            <div>{activeTab}</div>
+                            <button className="btn-default-hover">Agenda tu cita</button>
                         </div>
                     </div>
                 </div>
                 <div className="content-img">
-
+                    <style jsx>{`
+                        .content-img {
+                            background-image: url(${require("../../assets/facial.jpg")});
+                            background-size: cover;
+                            background-position: center;
+                        }
+                    `}</style>
                 </div>
             </div>
         </div>
--- components/tabs/tabs.js
+++ components/tabs/tabs.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-
+//import PropTypes from 'prop-types';
+import './tabs.css';
 
 class TabManager extends Component {
   state = {}
@@ -8,24 +8,25 @@ class TabManager extends Component {
   render() {
     const { activeTab, handleTab, tabs } = this.props;
     return (
-      <div className="tab-manager">
-        {tabs.map(({ label, value }) => (
-          <div
-            className={`tab ${value === activeTab ? 'selected-tab' : ''}`}
-            onClick={() => { handleTab(value); }}
-          >
-            {label}
-          </div>
-        ))}
-      </div>
+        <div className="tab-manager">
+            {tabs.map(({ label, value, title, text }) => (
+            <div
+                className={`tab ${value === activeTab ? 'selected-tab' : ''}`}
+                onClick={() => { handleTab([<h3>{title}</h3>, <p>{text}</p>]); }}
+                key={value}
+            >
+                <h3>{label}</h3>
+            </div>
+            ))}
+        </div>
     );
   }
 }
 
-TabManager.propTypes = {
+/*TabManager.propTypes = {
   activeTab: PropTypes.number.isRequired,
   handleTab: PropTypes.func.isRequired,
   tabs: PropTypes.arrayOf(Object).isRequired,
-};
+};*/
 
 export default TabManager;
